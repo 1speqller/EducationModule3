f37f2d3281a889252d6d00fb46d5ec62303911ea
1speqller <hoshigake2000@gmail.com>
2024-08-18 10:29:45 +0300

    Личное описание с использованием различных типов

--- AducationConsole.ConsoleApp/Program.cs
+++ AducationConsole.ConsoleApp/Program.cs
@@ -1,1 +1,6 @@
-﻿Console.WriteLine("\x23");
+﻿string name = "Milkhail";
+byte age = 21;
+bool isPetOwner = true;
+float footSize = 45;
+
+Console.WriteLine("My name is {0} \nMy age is {1} \nDo I have a pet? {2} \nMy shoe size is {3}", name, age, isPetOwner, footSize);
