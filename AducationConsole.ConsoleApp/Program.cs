f829696a586505f2848a37609fdc363e7e7ce6fc
1speqller <hoshigake2000@gmail.com>
2024-08-18 10:50:46 +0300

    Создание перечисления Semaphore

--- AducationConsole.ConsoleApp/Program.cs
+++ AducationConsole.ConsoleApp/Program.cs
@@ -1,6 +1,6 @@
-﻿string name = "Milkhail";
-byte age = 21;
-bool isPetOwner = true;
-float footSize = 45;
-
-Console.WriteLine("My name is {0} \nMy age is {1} \nDo I have a pet? {2} \nMy shoe size is {3}", name, age, isPetOwner, footSize);
+﻿enum Semaphore : int
+{
+    Red = 100,
+    Yellow = 200,
+    Green = 300
+}
