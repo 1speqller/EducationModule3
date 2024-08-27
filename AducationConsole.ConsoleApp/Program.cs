e22cd57b14e9d3e68a0d5b89bc0148b4a89c0129
1speqller <hoshigake2000@gmail.com>
2024-08-27 10:46:49 +0300

    Создание сортировки массива через буфферную переменную

--- AducationConsole.ConsoleApp/Program.cs
+++ AducationConsole.ConsoleApp/Program.cs
@@ -1,9 +1,21 @@
-﻿Console.Write("Введите своё имя: ");
-var name = Console.ReadLine();
+﻿var arr = new int[] { 5, 6, 9, 1, 2, 3, 4 };
 
-for (int i = name.Length - 1; i >= 0; i--)
+int buffValue;
+
+for (int i = 0; i < arr.Length; i++)
 {
-    Console.Write(name[i]);
+    for (int j = 1; j < arr.Length; j++)
+    {
+        if (arr[j-1] > arr[j])
+        {
+            buffValue = arr[j];
+            arr[j] = arr[j-1];
+            arr[j-1] = buffValue;
+        }    
+    }
 }
+
+foreach(var i in arr)
+    Console.Write(i + " ");
 
 Console.ReadKey();
