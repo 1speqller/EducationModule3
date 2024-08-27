13014f31ce9f07a92da38907f633827cfd65d10f
1speqller <hoshigake2000@gmail.com>
2024-08-27 11:24:03 +0300

    Анкетирвоание трех пользователей по заданным параметрам

--- AducationConsole.ConsoleApp/Program.cs
+++ AducationConsole.ConsoleApp/Program.cs
@@ -1,13 +1,33 @@
-﻿var arr = new int[] { -5, 6, -9, 1, -2, 3, 4 };
-int sum = 0;
+﻿(string FName, string LName, string Login, int LoginLength, bool IsPetOwner, int Age, string[] FavColors) User;
 
-for (int i = 0; i < arr.Length; i++)
+for (int i = 0; i < 3; i++)
 {
-    if (arr[i] > 0)
-        sum++;
+    Console.WriteLine("Введите имя");
+    User.FName = Console.ReadLine();
+
+    Console.WriteLine("Введите фамилию");
+    User.LName = Console.ReadLine();
+
+    Console.WriteLine("Введите логин");
+    User.Login = Console.ReadLine();
+    User.LoginLength = User.Login.Length;
+
+    Console.WriteLine("Есть ли у вас животные? Да или Нет");
+    string petAnswer = Console.ReadLine();
+
+    if (petAnswer == "Да")
+        User.IsPetOwner = true;
+
+    Console.WriteLine("Введите возраст пользователя");
+    User.Age = Convert.ToInt32(Console.ReadLine());
+
+    User.FavColors = new string[3];
+    Console.WriteLine("Введите три любимых цвета пользователя");
+
+    for (int j = 0; j < User.FavColors.Length; j++)
+    {
+        User.FavColors[j] = Console.ReadLine();
+    }
 }
-
-
-Console.WriteLine(sum);
 
 Console.ReadKey();
