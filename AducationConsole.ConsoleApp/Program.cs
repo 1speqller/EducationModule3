a544ff9187b7f10991b40559440d700235a6ad41
1speqller <hoshigake2000@gmail.com>
2024-08-30 10:59:01 +0300

    Создание метода вывода массива в консоль, с сортировкой в зависимости от условия

--- AducationConsole.ConsoleApp/Program.cs
+++ AducationConsole.ConsoleApp/Program.cs
@@ -1,50 +1,50 @@
-﻿string[] favcolors = new string[3];
-
-for (int i = 0; i < favcolors.Length; i++)
+﻿static int[] GetArrayFromConsole()
 {
-    favcolors[i] = ShowColor();
-}
+    var result = new int[5];
 
-foreach (string color in favcolors)
-    { 
-        Console.WriteLine(color); 
-    }
-
-Console.ReadKey();
-
-static string ShowColor()
-{
-    Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
-    var color = Console.ReadLine();
-
-    switch (color)
+    for (int i = 0; i < result.Length; i++)
     {
-        case "red":
-            Console.BackgroundColor = ConsoleColor.Red;
-            Console.ForegroundColor = ConsoleColor.Black;
-
-            Console.WriteLine("Your color is red!");
-            break;
-
-        case "green":
-            Console.BackgroundColor = ConsoleColor.Green;
-            Console.ForegroundColor = ConsoleColor.Black;
-
-            Console.WriteLine("Your color is green!");
-            break;
-        case "cyan":
-            Console.BackgroundColor = ConsoleColor.Cyan;
-            Console.ForegroundColor = ConsoleColor.Black;
-
-            Console.WriteLine("Your color is cyan!");
-            break;
-        default:
-            Console.BackgroundColor = ConsoleColor.Yellow;
-            Console.ForegroundColor = ConsoleColor.Red;
-
-            Console.WriteLine("Your color is yellow!");
-            break;
+        Console.WriteLine("Введите элемент массива номер {0}", i + 1);
+        result[i] = int.Parse(Console.ReadLine());
     }
 
-    return color;
+    return result;
 }
+
+static int[] ReturnSortArray(int[] array)
+{
+    int temp;
+    for (int i = 0; i < array.Length; i++)
+    {
+        for (int j = 0; j < array.Length; j++)
+        {
+            if (array[i] < array[j])
+            {
+                temp = array[j];
+                array[j] = array[i];
+                array[i] = temp;
+            }
+        }
+    }
+    return array;
+}
+
+static void ShowArray(int[] array, bool IsSort = false) 
+{
+    if (IsSort)
+    {
+        foreach (int i in ReturnSortArray(array))
+        {
+            Console.Write(i + " ");
+        }
+    }
+    else
+    {
+        foreach (int i in array)
+        {
+            Console.Write(i + " ");
+        }
+    }
+}
+
+ShowArray(GetArrayFromConsole(), true);
