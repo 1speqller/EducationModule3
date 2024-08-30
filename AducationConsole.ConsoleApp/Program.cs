790ff299e0b36b6adff20d261c886ef380df4259
1speqller <hoshigake2000@gmail.com>
2024-08-30 09:20:08 +0300

    Создание анкеты пользователя с использованием массива и последующим его заполнением через цикл

--- AducationConsole.ConsoleApp/Program.cs
+++ AducationConsole.ConsoleApp/Program.cs
@@ -1,33 +1,18 @@
-﻿(string FName, string LName, string Login, int LoginLength, bool IsPetOwner, int Age, string[] FavColors) User;
+﻿(string Name, string[] Dishes) User;
+User.Dishes = new string[5];
 
-for (int i = 0; i < 3; i++)
+Console.Write("Введите свое имя: ");
+User.Name  = Console.ReadLine();
+
+Console.WriteLine("Введите 5 любимых блюд: ");
+for (int i = 0; i < User.Dishes.Length; i++)
 {
-    Console.WriteLine("Введите имя");
-    User.FName = Console.ReadLine();
-
-    Console.WriteLine("Введите фамилию");
-    User.LName = Console.ReadLine();
-
-    Console.WriteLine("Введите логин");
-    User.Login = Console.ReadLine();
-    User.LoginLength = User.Login.Length;
-
-    Console.WriteLine("Есть ли у вас животные? Да или Нет");
-    string petAnswer = Console.ReadLine();
-
-    if (petAnswer == "Да")
-        User.IsPetOwner = true;
-
-    Console.WriteLine("Введите возраст пользователя");
-    User.Age = Convert.ToInt32(Console.ReadLine());
-
-    User.FavColors = new string[3];
-    Console.WriteLine("Введите три любимых цвета пользователя");
-
-    for (int j = 0; j < User.FavColors.Length; j++)
-    {
-        User.FavColors[j] = Console.ReadLine();
-    }
+    User.Dishes[i] = Console.ReadLine();
 }
+
+foreach (string Dishes in User.Dishes)
+    { 
+        Console.WriteLine(Dishes); 
+    }
 
 Console.ReadKey();
