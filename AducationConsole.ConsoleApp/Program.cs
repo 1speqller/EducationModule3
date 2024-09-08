ba91c78ef6c52b99ea19ada82d1bec65812e858d
1speqller <hoshigake2000@gmail.com>
2024-09-08 11:00:21 +0300

    Создание класса User с использованием свойств для определения полей класса

--- AducationConsole.ConsoleApp/Program.cs
+++ AducationConsole.ConsoleApp/Program.cs
@@ -1,15 +1,60 @@
-﻿class TrafficLight()
-{
-    private string color;
+﻿User user = new User();
+user.Email = "asdasdasd";
 
-    private void ChangeColor(string newColor)
+class User
+{
+    private int age;
+    private string login;
+    private string email;
+
+    public int Age
     {
-        if (color != newColor)
-            color = newColor;
+        get
+        {
+            return age;
+        }
+
+        set
+        {
+            if (value < 18)
+                Console.WriteLine("Возраст должен быть не меньше 18");
+            
+            else
+                age = value;
+            
+        }
     }
 
-    public string GetColor()
+    public string Login
     {
-        return color;
+        get
+        {
+            return login;
+        }
+        set
+        {
+            if (login.Length < 3)
+                Console.WriteLine("Поле логин должно иметь длину больше 3-х символов!");
+
+            else
+                login = value;
+        }
+    }
+
+    public string Email
+    {
+        get
+        {
+            return email;
+        }
+        set
+        {
+            if (email.Contains('@'))
+                email = value;
+                
+
+            else
+                Console.WriteLine("Поле email должно включать символ @");
+        }
     }
 }
