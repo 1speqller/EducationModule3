c5227f9b94555bc1d5ee98b3b2ccb422411f6aa6
1speqller <hoshigake2000@gmail.com>
2024-09-15 09:46:41 +0300

    Создание 2 наследующих классов

--- AducationConsole.ConsoleApp/Program.cs
+++ AducationConsole.ConsoleApp/Program.cs
@@ -1,60 +1,16 @@
-﻿User user = new User();
-user.Email = "asdasdasd";
-
-class User
+﻿class Employee
 {
-    private int age;
-    private string login;
-    private string email;
+    public string Name;
+    public int Age;
+    public int Salary;
+}
 
-    public int Age
-    {
-        get
-        {
-            return age;
-        }
+class ProjectManager : Employee
+{
+    public string ProjectName;
+}
 
-        set
-        {
-            if (value < 18)
-                Console.WriteLine("Возраст должен быть не меньше 18");
-            
-            else
-                age = value;
-            
-        }
-    }
-
-    public string Login
-    {
-        get
-        {
-            return login;
-        }
-        set
-        {
-            if (login.Length < 3)
-                Console.WriteLine("Поле логин должно иметь длину больше 3-х символов!");
-
-            else
-                login = value;
-        }
-    }
-
-    public string Email
-    {
-        get
-        {
-            return email;
-        }
-        set
-        {
-            if (email.Contains('@'))
-                email = value;
-                
-
-            else
-                Console.WriteLine("Поле email должно включать символ @");
-        }
-    }
+class Developer : Employee
+{
+    public string ProgrammingLanguage;
 }
