566ed2482f70fb18ee7ac1b6a1a2f1e10008c5a2
1speqller <hoshigake2000@gmail.com>
2024-09-20 12:33:38 +0300

    Создание структуры классов по итоговому заданию модуля 7

--- AducationConsole.ConsoleApp/Program.cs
+++ AducationConsole.ConsoleApp/Program.cs
@@ -1,23 +1,109 @@
-﻿abstract class CarEngine { }
-
-class ElectricEngine : CarEngine { }
-
-class GasEngine : CarEngine { }
-
-abstract class CarPart { }
-
-class Battery : CarPart { }
-
-class Differential : CarPart { }
-
-class Wheel : CarPart { }
-
-class Car<TEngine> where TEngine : CarEngine
+﻿abstract class Delivery
 {
-    public TEngine Engine;
+    public string Address;
+}
 
-    public virtual void ChangePart<TCarPart>(TCarPart newPart) where TCarPart : CarPart
+class HomeDelivery : Delivery
+{
+    
+}
+
+class PickPointDelivery : Delivery
+{
+    
+}
+
+class ShopDelivery : Delivery
+{
+    
+}
+
+class Product<TProductType> where TProductType : ProductType
+{
+    public int Id
     {
+        set 
+        {
+            if (value > 0)
+                Id = value;
+        }
+    }
+    public string Name
+    {
+        get { return Name; }
+        set 
+        {
+            if (value.Length != 0 && value != null)
+                Name = value;
 
+            else Console.WriteLine();
+        }
+    }
+    public string Description;
+    public decimal Price;
+
+    protected TProductType Type;
+}
+
+//Тип продукта (Еда, продукты для дома и т.д.)
+class ProductType
+{
+    public int Id;
+    public string Name;
+}
+
+class ProductCart<TProduct> where TProduct : Product<ProductType>
+{
+    public int Id;
+    public List<TProduct> Products;
+    public decimal CartPrice;
+
+    public void CalculatingPrice(List<TProduct> products) 
+    {
+        foreach (Product<ProductType> p in products)
+        {
+            CartPrice += p.Price;
+        }
+    }
+
+    public void AddProduct(TProduct product) { }
+    public void RemoveProduct(int id) { }
+}
+
+abstract class Coupon
+{
+    public int Percent;
+    public virtual void ApplyCoupon() { }
+}
+
+//Купон, который применяется к определенному продукту
+class CouponProduct<TProduct> : Coupon where TProduct : Product<ProductType>
+{
+    public int Id;
+    private TProduct Product;
+
+    public override void ApplyCoupon() { }
+}
+
+//Купон, который применяется к корзине целиком
+class CouponCart<TCart> : Coupon where TCart : ProductCart<Product<ProductType>>
+{
+    public int Id;
+    private TCart Cart;
+
+    public override void ApplyCoupon() { }
+
+    class Order<TDelivery, TStruct, TProduct> where TDelivery : Delivery where TProduct : Product<ProductType>
+    {
+        public int Id;
+        public TDelivery Delivery;
+        public string Description;
+
+
+
+        public void DisplayAddress()
+        {
+            Console.WriteLine(Delivery.Address);
+        }
     }
 }
