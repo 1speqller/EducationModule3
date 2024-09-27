771ffc58efbcd72e00d16b6d6814bde2a0af9f1c
1speqller <hoshigake2000@gmail.com>
2024-09-27 05:30:06 +0300

    Создание файловой структуры "своими силами"

--- AducationConsole.ConsoleApp/Program.cs
+++ AducationConsole.ConsoleApp/Program.cs
@@ -1,109 +1,25 @@
-﻿abstract class Delivery
+﻿public class Drive
 {
-    public string Address;
-}
-
-class HomeDelivery : Delivery
-{
-    
-}
-
-class PickPointDelivery : Delivery
-{
-    
-}
-
-class ShopDelivery : Delivery
-{
-    
-}
-
-class Product<TProductType> where TProductType : ProductType
-{
-    public int Id
+    public Drive(string name, long totalSpace, long freeSpace)
     {
-        set 
-        {
-            if (value > 0)
-                Id = value;
-        }
-    }
-    public string Name
-    {
-        get { return Name; }
-        set 
-        {
-            if (value.Length != 0 && value != null)
-                Name = value;
-
-            else Console.WriteLine();
-        }
-    }
-    public string Description;
-    public decimal Price;
-
-    protected TProductType Type;
-}
-
-//Тип продукта (Еда, продукты для дома и т.д.)
-class ProductType
-{
-    public int Id;
-    public string Name;
-}
-
-class ProductCart<TProduct> where TProduct : Product<ProductType>
-{
-    public int Id;
-    public List<TProduct> Products;
-    public decimal CartPrice;
-
-    public void CalculatingPrice(List<TProduct> products) 
-    {
-        foreach (Product<ProductType> p in products)
-        {
-            CartPrice += p.Price;
-        }
+        Name = name;
+        TotalSpace = totalSpace;
+        FreeSpace = freeSpace;
     }
 
-    public void AddProduct(TProduct product) { }
-    public void RemoveProduct(int id) { }
+    public string Name { get; }
+    public long TotalSpace { get; }
+    public long FreeSpace { get; }
 }
 
-abstract class Coupon
+public class Folder
 {
-    public int Percent;
-    public virtual void ApplyCoupon() { }
-}
+    public List<string> Files { get; set; } = new List<string>();
 
-//Купон, который применяется к определенному продукту
-class CouponProduct<TProduct> : Coupon where TProduct : Product<ProductType>
-{
-    public int Id;
-    private TProduct Product;
-
-    public override void ApplyCoupon() { }
-}
-
-//Купон, который применяется к корзине целиком
-class CouponCart<TCart> : Coupon where TCart : ProductCart<Product<ProductType>>
-{
-    public int Id;
-    private TCart Cart;
-
-    public override void ApplyCoupon() { }
-
-    class Order<TDelivery, TStruct, TProduct> where TDelivery : Delivery where TProduct : Product<ProductType>
+    Dictionary<string, Folder> Folders = new Dictionary<string, Folder>();
+    public void CreateFolder(string name)
     {
-        public int Id;
-        public TDelivery Delivery;
-        public string Description;
-
-
-
-        public void DisplayAddress()
-        {
-            Console.WriteLine(Delivery.Address);
-        }
+        Folders.Add(name, new Folder());
     }
+
 }
